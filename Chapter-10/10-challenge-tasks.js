6cfe683f828a769bf29e587b99b3efbbe33e77cf
abhinay1249 <abhinaymarise@gmail.com>
2025-10-22 23:40:38 +0530

    Initial Commit

--- Chapter-10/10-challenge-tasks.js
+++ Chapter-10/10-challenge-tasks.js
@@ -13,17 +13,17 @@
                     gamingButton.classList.remove("is-toggled");
                 }
             }
-            function onToggle1(){
-                if(!gamingButton1.classList.contains("is-toggled")){
-                    gamingButton1.classList.add("is-toggled");
-                }else{
-                    gamingButton1.classList.remove("is-toggled");
-                }
-            }
-            function onToggle2(){
-                if(!gamingButton2.classList.contains("is-toggled")){
-                    gamingButton2.classList.add("is-toggled");
-                }else{
-                    gamingButton2.classList.remove("is-toggled");
-                }
-            }
+            // function onToggle1(){
+            //     if(!gamingButton1.classList.contains("is-toggled")){
+            //         gamingButton1.classList.add("is-toggled");
+            //     }else{
+            //         gamingButton1.classList.remove("is-toggled");
+            //     }
+            // }
+            // function onToggle2(){
+            //     if(!gamingButton2.classList.contains("is-toggled")){
+            //         gamingButton2.classList.add("is-toggled");
+            //     }else{
+            //         gamingButton2.classList.remove("is-toggled");
+            //     }
+            // }
