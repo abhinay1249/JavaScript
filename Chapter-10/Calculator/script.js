02ee8d04b4380fa61a0fa99418bd018646fdac34
abhinay1249 <abhinaymarise@gmail.com>
2025-10-23 21:42:06 +0530

    Initial Commit

---
+++ Chapter-10/Calculator/script.js
@@ -0,0 +1,15 @@
+      let calculation = localStorage.getItem('calculation') || '';
+
+      function updateCalculation(value) {
+        calculation += value;
+        showCalculation();
+        localStorage.setItem('calculation', calculation);
+      }
+
+      function saveCalculation() {
+        localStorage.setItem('calculation', calculation);
+      }
+
+      function showCalculation(){
+        document.querySelector('.js-calculation').innerHTML=calculation;
+      }
