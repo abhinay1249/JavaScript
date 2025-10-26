a9813d44f969952769c6e46c129d1a0d04fba98c
abhinay1249 <abhinaymarise@gmail.com>
2025-10-26 16:33:04 +0530

    Add Event Listener

--- Chapter-12/Todo-List-ForEach/12-todo-list.js
+++ Chapter-12/Todo-List-ForEach/12-todo-list.js
@@ -11,18 +11,28 @@ function rendertodoList(){
         const htmlValues = `
             <div> ${name}</div>
             <div> ${date} </div>
-                <button     
-                    onclick="todolist.splice(${index},1)
-                    rendertodoList();
-                    localStorage.setItem('todolist',JSON.stringify(todolist));"
-                    class="css-delete">Delete
+                <button 
+                    class="css-delete js-delete">Delete
                 </button>
         `;
         todoHtml+=htmlValues;
     })
     document.querySelector(".js-div-tag").innerHTML=todoHtml;
+
+    document.querySelectorAll('.js-delete').forEach((deleteButton,index)=>{
+        deleteButton.addEventListener('click',()=>{
+            todolist.splice(index,1)
+            rendertodoList();
+            localStorage.setItem('todolist',JSON.stringify(todolist));
+        })
+    });
+    
    // console.log(todoHtml);
 }
+
+document.querySelector('.js-add').addEventListener('click',() => {
+    addTodoList();
+});
 
 function addTodoList(){
     const input_value = document.querySelector(".js-input-value");
