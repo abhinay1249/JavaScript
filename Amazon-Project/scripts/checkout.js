3ca3e2f63153640b547919a4415fe4bf22c82080
abhinay1249 <abhinaymarise@gmail.com>
2025-12-07 14:43:04 +0530

    Updates

--- Amazon-Project/scripts/checkout.js
+++ Amazon-Project/scripts/checkout.js
@@ -17,7 +17,7 @@ cart.forEach((cartItem)=>{
 
     
     cartSummaryHTML += `
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -103,6 +103,8 @@ document.querySelectorAll('.js-delete-link')
     item.addEventListener('click',()=>{
       const productId = item.dataset.productId;
       removeFromCart(productId);
-      console.log(cart);
+      
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+      console.log(container);
     });
   });
