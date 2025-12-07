37a578b222146e364f26fec8cd50105379a2f8ba
abhinay1249 <abhinaymarise@gmail.com>
2025-12-07 12:47:19 +0530

    Updates

--- Amazon-Project/scripts/checkout.js
+++ Amazon-Project/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -41,7 +41,7 @@ cart.forEach((cartItem)=>{
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary js-delete-link data-product-attribute=${productId}">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -101,7 +101,8 @@ document.querySelector('.js-order-summary').innerHTML=cartSummaryHTML;
 document.querySelectorAll('.js-delete-link')
   .forEach((item)=>{
     item.addEventListener('click',()=>{
-      const {productId} = item.dataset;
-      
-    })
-  })
+      const productId = item.dataset.productId;
+      removeFromCart(productId);
+      console.log(cart);
+    });
+  });
