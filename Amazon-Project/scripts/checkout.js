a872f7603e851f48a02494b5a7568f3a9f40a911
abhinay1249 <abhinaymarise@gmail.com>
2025-12-17 23:17:29 +0530

    External Modules

--- Amazon-Project/scripts/checkout.js
+++ Amazon-Project/scripts/checkout.js
@@ -23,10 +23,23 @@ cart.forEach((cartItem) => {
     }
   });
 
+  const deliveryOptionId = cartItem.deliverOptionId;
+
+  let deliveryOption;
+
+  deliveryOptions.forEach((option)=>{
+      if(option.id===deliveryOptionId){
+        deliveryOption=option;
+      }
+  });
+  const today = dayjs();
+  const deliveryDate=today.add(deliveryOption.deliveryDays,'days');
+  const dateString = deliveryDate.format('dddd, MMMM D');
+
   cartSummaryHTML += `
         <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
-              Delivery date: Tuesday, June 21
+              Delivery date: ${dateString}
             </div>
 
             <div class="cart-item-details-grid">
@@ -74,7 +87,7 @@ function deliveryOptionsHTML(matchingProduct,cartItem) {
   deliveryOptions.forEach((deliveryOption) => {
     const today = dayjs();
     const deliveryDate=today.add(deliveryOption.deliveryDays,'days');
-    const dateString = deliveryDate.format('dddd, MMMM D')
+    const dateString = deliveryDate.format('dddd, MMMM D');
     
     const priceString = deliveryOption.priceCents===0
     ? 'FREE'
