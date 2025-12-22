074b4e5338312da5e97702712f8a9498c4938ee7
abhinay1249 <abhinaymarise@gmail.com>
2025-12-22 22:56:24 +0530

    Initial Commit

--- Amazon-Project/scripts/checkout/paymentSummary.js
+++ Amazon-Project/scripts/checkout/paymentSummary.js
@@ -51,5 +51,6 @@ export function renderPaymentSummary(){
           <button class="place-order-button button-primary">
             Place your order
           </button>`
+          
     document.querySelector('.js-payment-summary').innerHTML=paymentSummaryHTML;
 }
