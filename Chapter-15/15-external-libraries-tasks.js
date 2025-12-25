f8e97c1418f56992997c1e0c8b0595d2403591de
abhinay1249 <abhinaymarise@gmail.com>
2025-12-25 11:23:20 +0530

    Initial Commit

--- Chapter-15/15-external-libraries-tasks.js
+++ Chapter-15/15-external-libraries-tasks.js
@@ -1,7 +1,18 @@
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 
+// 1st Task
+
 const today = dayjs();
 const next5days = today.add(5,'days');
 const date = next5days.format('MMMM D');
 
+// 2nd task
+
+const next1month = today.add(1,'month');
+const date_1 = next1month.format('MMMM D');
+
+
+
+
 console.log(date);
+console.log(date_1);
