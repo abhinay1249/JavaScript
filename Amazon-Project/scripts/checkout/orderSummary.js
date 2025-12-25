1c120a1023fa21073dd6cc87f9106075b0d9c5bd
abhinay1249 <abhinaymarise@gmail.com>
2025-12-25 13:28:01 +0530

    Updated 15-tasks-k

--- Amazon-Project/scripts/checkout/orderSummary.js
+++ Amazon-Project/scripts/checkout/orderSummary.js
@@ -4,6 +4,8 @@ import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 import { renderPaymentSummary } from './paymentSummary.js';
+import { renderCheckoutHeader } from './checkoutHeader.js';
+
 
 // Checking out the External Libraries.
 // const temp = dayjs();
@@ -117,6 +119,7 @@ export function renderOrderSummary(){
 
         // const container = document.querySelector(`.js-cart-item-container-${productId}`);
         // container.remove();
+        renderCheckoutHeader();
         renderOrderSummary();
         renderPaymentSummary();
       });
