7c9ebf69f8993c19205863ad9193de8d33e76abd
abhinay1249 <abhinaymarise@gmail.com>
2025-12-25 11:39:26 +0530

    Initial Commit

--- Chapter-15/15-external-libraries-tasks.js
+++ Chapter-15/15-external-libraries-tasks.js
@@ -6,13 +6,27 @@ const today = dayjs();
 const next5days = today.add(5,'days');
 const date = next5days.format('MMMM D');
 
+console.log(date);
+
 // 2nd task
 
-const next1month = today.add(1,'month');
+const next1month = today.add(1,'months');
 const date_1 = next1month.format('MMMM D');
 
-
-
-
-console.log(date);
 console.log(date_1);
+
+// 3rd task
+
+const back1month = today.subtract(1,'months');
+const date_2 = back1month.format('MMMM D');
+
+console.log(date_2);
+
+// 4th task
+
+const dayofweek = today.format('dddd');
+
+console.log(dayofweek);
+
+// 5th task
+
