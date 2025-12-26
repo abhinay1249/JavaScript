b7008fdffdeeeea799d6db4e5bae2cf8bda57c0c
abhinay1249 <abhinaymarise@gmail.com>
2025-12-26 11:57:50 +0530

    Testing Frameworks

--- Amazon-Project/scripts/checkout/orderSummary.js
+++ Amazon-Project/scripts/checkout/orderSummary.js
@@ -1,7 +1,6 @@
 import { cart, removeFromCart, calculateCartQuantity, updateQuantity,updateDeliveryOption} from '../../data/cart.js';
 import { getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
-import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {calculateDeliveryDate, deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 import { renderPaymentSummary } from './paymentSummary.js';
 import { renderCheckoutHeader } from './checkoutHeader.js';
---
+++ Amazon-Project/tests/moneyTest.js
@@ -0,0 +1,27 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+console.log("Test Suite: Format Currency");
+
+
+if(formatCurrency(2095)==='20.95'){
+    console.log("passed");
+}else{
+    console.log("failed");
+}
+
+console.log("Works with 0");
+
+if(formatCurrency(0)==='0.00'){
+    console.log("passed");
+}else{
+    console.log("failed");
+}
+
+console.log("Rounds up to zero");
+
+if(formatCurrency(2000.5)==='20.01'){
+    console.log('passed');
+}else{
+    console.log("failed");
+}
+
