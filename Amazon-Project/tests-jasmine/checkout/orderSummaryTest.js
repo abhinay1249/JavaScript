e5c178af0d87ae98cd8646673d2fba7233f6995b
abhinay1249 <abhinaymarise@gmail.com>
2025-12-26 16:35:00 +0530

    Jasmine Testing Framework

--- Amazon-Project/tests-jasmine/checkout/orderSummaryTest.js
+++ Amazon-Project/tests-jasmine/checkout/orderSummaryTest.js
@@ -39,4 +39,27 @@ describe('Test Suite: renderOrderSummary', () => {
             document.querySelector(`.js-product-quantity-${productId2}`).innerText
         ).toContain('Quantity: 1');
     });
+
+    it('Removes a Product',()=>{
+        document.querySelector('.js-test-container').innerHTML = `
+            <div class="js-order-summary"></div>
+            <div class="js-checkout-quantity"></div>
+        `;
+        const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
+        const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([{
+                productId: productId1,
+                quantity: 2,
+                deliveryOptionId: '1'
+            }, {
+                productId: productId2,
+                quantity: 1,
+                deliveryOptionId: '2'
+            }]);
+        });
+        loadFromStorage();
+        
+        renderOrderSummary();
+    });
 });
