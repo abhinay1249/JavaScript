7a158a82735c088c87cd37f9bb5d171f57df7449
abhinay1249 <abhinaymarise@gmail.com>
2025-12-26 14:29:47 +0530

    Jasmine Testing Framework

--- Amazon-Project/tests-jasmine/moneyTest.js
+++ Amazon-Project/tests-jasmine/moneyTest.js
@@ -5,4 +5,10 @@ describe('Test Suite: Format Currency',()=>{
     it('Convert Cents to Dollars',()=>{
         expect(formatCurrency(2095)).toEqual('20.95');
     });
+    it('Works with Zero',()=>{
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
+    it('Rounds Up to Nearest Cent',()=>{
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
 });
