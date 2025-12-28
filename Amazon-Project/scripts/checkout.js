7c76fd64b694b0fef075531b203acb521232b5b0
abhinay1249 <abhinaymarise@gmail.com>
2025-12-28 13:45:49 +0530

    Backend

--- Amazon-Project/scripts/checkout.js
+++ Amazon-Project/scripts/checkout.js
@@ -6,21 +6,20 @@ import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
+Promise.all([
+    new Promise((resolve) =>{
+        loadProducts(() =>{
+            resolve('promise-1');
+        });
+    }),
 
-
-new Promise((resolve) =>{
-    loadProducts(() =>{
-        resolve();
-    });
-    
-}).then(() =>{
     new Promise((resolve) =>{
         loadCart(()=>{
-            resolve();
+            resolve('promise-2');
         });
-    });
-
-}).then(()=>{
+    })
+]).then((values)=>{
+    console.log(values);
     renderCheckoutHeader();
     renderOrderSummary();
     renderPaymentSummary();
