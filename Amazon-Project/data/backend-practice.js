97f76538178356277a8cef71a92345f36b2b1266
abhinay1249 <abhinaymarise@gmail.com>
2025-12-28 12:06:52 +0530

    Backend

--- Amazon-Project/data/backend-practice.js
+++ Amazon-Project/data/backend-practice.js
@@ -1,5 +1,8 @@
 const xhr = new XMLHttpRequest();
 
-xhr.open('GET','https://supersimplebackend.dev');
+xhr.addEventListener('load',()=>{
+    console.log(xhr.response);
+});
 
+xhr.open('GET','https://supersimplebackend.dev');
 xhr.send();
